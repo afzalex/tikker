6eab710af3b95b36be3f952b3b753db7e81af7de
Mohammad Afzal <mafzal@altimetrik.com>
2021-10-18 01:25:04 +0530

    Added more functionalities

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 ### Copy below in console for tiktok
 
 ```js
-fetch("https://afzalex.github.io/tikker/tiktok-downloader-script.js?v=03")
+fetch("https://afzalex.github.io/tikker/tiktok-downloader-script.js?v=04")
     .then(response => response.text())
     .then(text => eval(text));
 ```
--- tiktok-downloader-script.js
+++ tiktok-downloader-script.js
@@ -1,6 +1,6 @@
 (function () {
 
-    function tikFunction(itemList, dataPointer) {
+    function tikFunction(itemList, dataPointer, downloaded) {
         let loadScript = this.loadScript = function (url, onload) {
             let script = document.createElement("script");
             script.setAttribute("src", url);
@@ -11,7 +11,7 @@
 
         loadScript("https://code.jquery.com/jquery-3.6.0.js")
 
-        let download = this.download = function (url, filename) {
+        let download = this.download = function (url, filename, successHandler, errorHandler) {
             fetch(url).then(data => {
                 const blob = data.blob();
                 return blob;
@@ -25,32 +25,44 @@
                     document.body.appendChild(elem);
                     elem.click();
                     document.body.removeChild(elem);
+                    if(successHandler) successHandler();
                 };
                 reader.readAsDataURL(blob);
+            }).catch(err => {
+                if(errorHandler) errorHandler(err);
             })
         }
 
         this.dataPointer = 0;
+        this.itemList = [];
+        this.downloaded = [];
+
         if (dataPointer) {
             this.dataPointer = dataPointer;
         }
 
-
-        this.itemList = []
         if (itemList) {
             this.itemList = itemList;
         }
 
-        this.downloaded = [];
-        this.downloadNextVideo = function (force) {
+        if (downloaded) {
+            this.downloaded = downloaded;
+        }
+
+        this.downloadNextVideo = function (force, successHandler, errorHandler) {
             let downloaded = this.downloaded;
             let itemList = this.itemList;
             let dataPointer = this.dataPointer;
             if (dataPointer < itemList.length) {
                 let item = itemList[dataPointer];
-                if (force || downloaded.indexOf(item.id) == -1) {
-                    download(item.video.downloadAddr, item.id, () => pointerDownloader(++dataPointer))
-                    downloaded.push(item.id);
+                let isNewToDownload = downloaded.indexOf(item.id) == -1;
+                if (force || isNewToDownload) {
+                    download(item.video.downloadAddr, item.id, () => {
+                        successHandler && successHandler();
+                        isNewToDownload && downloaded.push(item.id);
+                    }, e => {
+                        console.log(`%c Unable to download video of dataPointer ${dataPointer}`, 'color: #a00')
+                    });
                 } else {
                     console.log('%c Present in downloaded list.', 'color: #a00');
                 }
@@ -64,8 +76,18 @@
 
         this.redownloadLastVideo = function () {
             this.dataPointer--;
-            let item = itemList[this.dataPointer];
+            let item = this.itemList[this.dataPointer];
             this.downloadNextVideo(true);
+        }
+
+        this.downloadNextNVideos = function(n, force) {
+            if(!n) n = 10;
+            for(let i = 0; i < n; i++) tik.downloadNextVideo(force)
+        }
+        this.redownloadNextNVideos = function(n, force) {
+            if(!n) n = 10;
+            tik.dataPointer -= n;
+            this.downloadNextNVideos(n, force);
         }
 
 
@@ -152,12 +174,19 @@
             downloadObjectAsJson(this.itemList);
         }
     }
-
-    if (typeof window.itemList === 'undefined') {
-        window.itemList = []
+    let tik = new tikFunction();
+    if (window.tik) {
+        if (window.tik.dataPointer) {
+            tik.dataPointer = window.tik.dataPointer;
+        }
+        if (window.tik.itemList) {
+            tik.itemList = window.tik.itemList;
+        }
+        if (window.tik.downloaded) {
+            tik.downloaded = window.tik.downloaded;
+        }
     }
-    window.tik = new tikFunction(itemList);
-    window.tik.setDataPointer(0);
+    window.tik = tik;
 
 
     console.log("%c tik is installed successfully.", "color: #070; font-weight: bold;");
